4b17bed91ed3e5203b84cbe12b9d3e0f4e42241c
David <langst02@email.franklin.edu>
2017-12-28 07:36:50 -0500

    Adjust screen placement

--- setDisplay.sh
+++ setDisplay.sh
@@ -9,11 +9,16 @@ LaptopDisplay="LVDS1"
 LEFT="DVI-I-2-1"
 #RIGHT="DVI-I-3-3"
 #RIGHT="DVI-I-2-3"
-RIGHT="VGA1"
+VGA_PORT="VGA1"
 
 #LMODE="1680x1850"
 LMODE="1680x1050"
 RMODE="1920x1080"
+
+#Location
+LAPTOP_POS="0x282"
+LEFT_POS="1366x0"
+RIGHT_POS="3046x0"
 
 for OUTPUT in $(xrandr --listproviders | awk  '/modesetting/ {print $2 | "tr -d [:punct:]"}');do
 	xrandr --setprovideroutputsource $OUTPUT 0
@@ -21,4 +26,5 @@ for OUTPUT in $(xrandr --listproviders | awk  '/modesetting/ {print $2 | "tr -d 
 done
 
 #xrandr --output $LEFT --mode $LMODE --right-of $LaptopDisplay --output $RIGHT --auto --right-of $LEFT
-xrandr --output $LEFT --auto --right-of $LaptopDisplay --output $RIGHT --auto --right-of $LEFT
+#xrandr --output $LEFT --auto --right-of $LaptopDisplay --output $RIGHT --auto --right-of $LEFT
+xrandr --output $LEFT --auto --pos $LEFT_POS --output $VGA_PORT --off
